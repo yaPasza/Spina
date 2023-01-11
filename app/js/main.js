882948933f53a133d0d595726f9c9c741bcf54e5
Pavel Karpov <paszakarpow@yandex.ru>
2023-01-11 18:25:42 +0500

    pre-alpha version

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,46 @@
-$function({
+$(function () {
+
+    $('.slider-top').slick({
+        infinite: true,
+        arrows: false,
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings:{
+                    slidesToShow:3
+                }
+            },{
+                breakpoint: 480,
+                settings:{
+                    slidesToShow:2
+                }
+            }
+        ]
+    });
+
+    $('.slider-bottom').slick({
+        infinite: true,
+        arrows: false,
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        rtl: true,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings:{
+                    slidesToShow:3
+                }
+            },{
+                breakpoint: 480,
+                settings:{
+                    slidesToShow:2
+                }
+            }
+        ]
+    });
 
 });
